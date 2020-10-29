8e96a459dcf6ba65a66fe2056be2662b01efcd10
Saumiya <saumiya1994@gmail.com>
2020-10-29 21:11:40 +0530

    Update employee

--- src/main/java/com/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +25,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public void saveEmployee(Employee employee) {
 		this.employeeRepository.save(employee);
 	}
+	
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		if (optional.isPresent()) {
+			employee = optional.get();
+		} else {
+			throw new RuntimeException(" Employee not found for id :: " + id);
+		}
+		return employee;
+	}
+
 }
