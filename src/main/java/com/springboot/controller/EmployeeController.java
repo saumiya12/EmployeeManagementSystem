5e31958f59d97f7f97af8f4a8485c1e2310789a7
Saumiya <saumiya1994@gmail.com>
2020-10-30 14:07:54 +0530

    Pagination

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springboot.model.Employee;
 import com.springboot.service.EmployeeService;
@@ -19,8 +23,8 @@ public class EmployeeController {
 	
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
-		model.addAttribute("listEmployees", employeeService.getAllEmployees());
-		return "index";
+		return findPaginated(1, model);	
+		
 	}
 
 	@GetMapping("/showNewEmployeeForm")
@@ -55,5 +59,19 @@ public class EmployeeController {
 		// call delete employee method 
 		this.employeeService.deleteEmployeeById(id);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/page/{pageNo}")
+	public String findPaginated(@PathVariable (value = "pageNo") int pageNo, Model model) {
+		int pageSize = 5;
+		
+		Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+		List<Employee> listEmployees = page.getContent();
+		
+		model.addAttribute("currentPage", pageNo);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listEmployees", listEmployees);
+		return "index";
 	}
 }
--- src/main/java/com/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/service/EmployeeServiceImpl.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.springboot.model.Employee;
@@ -43,4 +47,10 @@ public class EmployeeServiceImpl implements EmployeeService {
 		this.employeeRepository.deleteById(id);
 	}
 
+	@Override
+	public Page<Employee> findPaginated(int pageNo, int pageSize) {
+
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+		return this.employeeRepository.findAll(pageable);
+	}
 }
